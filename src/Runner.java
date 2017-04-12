e7379def46613f171ea2fc868cb71b9fac3f2699
Anna Isabel Lamers <isabel.lamers@gmail.com>
2017-04-12 17:40:40 +0200

    Testing with a first java app

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+/**
+ * Created by isabellamers on 12/04/17.
+ */
+public class Runner {
+    public static void main (String[] args) {
+        System.out.println("*");
+
+        for (int x = 0; x < 10; x = x + 1) {
+            System.out.println("x is now" + x);
+        }
+    }
+}
