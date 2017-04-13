d105a2d9200b3193052dcdefc70cbc80e9f2f8a1
Anna Isabel Lamers <isabel.lamers@gmail.com>
2017-04-13 21:22:59 +0200

    draw centred triangle with asterisk and spaces

--- src/Runner.java
+++ src/Runner.java
@@ -7,7 +7,6 @@ public class Runner {
     public static void main (String[] args) {
         System.out.println("*");
 
-
         int n;
         for (n = 0; n < 8; n = n + 1) {
             System.out.print("*");
@@ -21,7 +20,17 @@ public class Runner {
             for (int i = 0; i < n; i = i + 1) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.print("\n");
+        }
+
+        for (n = 0; n < 3; n++) {
+            for (int e = 3; e > n ; e--) {
+                System.out.print(" ");
+            }
+            for (int s = 1; s <= n + 1; s++) {
+                System.out.print(" *");
+            }
+            System.out.print("\n");
         }
 
     }
