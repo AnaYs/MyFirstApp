cbd7ec4ce96d5b79a506fa188167ee82f13af89b
Anna Isabel Lamers <isabel.lamers@gmail.com>
2017-04-13 21:12:29 +0200

    draw right triangle with asterisk

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.StringUtils;
+
 /**
  * Created by isabellamers on 12/04/17.
  */
@@ -10,7 +12,17 @@ public class Runner {
         for (n = 0; n < 8; n = n + 1) {
             System.out.print("*");
         }
-        
+
+        for (n = 0; n <= 3; n = n + 1) {
+            System.out.println("*");
+        }
+
+        for (n = 0; n <= 3; n = n + 1) {
+            for (int i = 0; i < n; i = i + 1) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
     }
 }
