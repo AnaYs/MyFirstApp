81784f1e6f4e1cba6949b4d43fbf39c0a77322c1
Anna Isabel Lamers <isabel.lamers@gmail.com>
2017-04-14 08:41:21 +0200

    draw diamond with name in centre

--- src/Runner.java
+++ src/Runner.java
@@ -45,7 +45,7 @@ public class Runner {
             for (c = 1; c <= s; c++) {
                 System.out.print(" ");
             }
-            spaces--;
+            s--;
             for (c = 1; c <= 2*r-1; c++) {
                 System.out.print("*");
             }
@@ -62,8 +62,34 @@ public class Runner {
             }
             System.out.println("");
         }
-
-
+        //diamond with name
+        n = 3;
+        s = n - 1;
+        for (r = 1; r <= n; r++) {
+            if (r < n) {
+                for (c = 1; c <= s; c++) {
+                    System.out.print(" ");
+                }
+                s--;
+                for (c = 1; c <= 2 * r - 1; c++) {
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            } else {
+                System.out.println("Anays");
+            }
+        }
+        s= 1;
+        for (r = 1; r <= n - 1; r++) {
+            for (c = 1; c <= s; c++) {
+                System.out.print(" ");
+            }
+            s++;
+            for (c = 1; c<= 2*(n-r)-1; c++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
     }
 }
