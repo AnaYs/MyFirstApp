825bead863c6e52340bc4e1199be18b7a19272d2
Anna Isabel Lamers <isabel.lamers@gmail.com>
2017-04-14 08:31:29 +0200

    diamond exercise

--- src/Runner.java
+++ src/Runner.java
@@ -8,30 +8,62 @@ public class Runner {
         System.out.println("*");
 
         int n;
+        //horizontal line
         for (n = 0; n < 8; n = n + 1) {
             System.out.print("*");
         }
-
+        //vertical line
         for (n = 0; n <= 3; n = n + 1) {
             System.out.println("*");
         }
-
+        //right triangle
         for (n = 0; n <= 3; n = n + 1) {
             for (int i = 0; i < n; i = i + 1) {
                 System.out.print("*");
             }
             System.out.print("\n");
         }
-
-        for (n = 0; n < 3; n++) {
-            for (int e = 3; e > n ; e--) {
+        //isosceles triangle
+        n = 3;
+        int count, row, spaces = 1;
+        spaces = n - 1;
+        for (row = 1; row <= n; row++) {
+            for(count = 1; count <= spaces; count++) {
                 System.out.print(" ");
             }
-            for (int s = 1; s <= n + 1; s++) {
-                System.out.print(" *");
+            spaces--;
+            for(count = 1; count <= 2*row-1; count++){
+                System.out.print("*");
             }
             System.out.print("\n");
         }
+        //diamond
+        n = 3;
+        int c, r, s = 1;
+        s = n - 1;
+        for (r = 1; r <=n; r++) {
+            for (c = 1; c <= s; c++) {
+                System.out.print(" ");
+            }
+            spaces--;
+            for (c = 1; c <= 2*r-1; c++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        s= 1;
+        for (r = 1; r <= n - 1; r++) {
+            for (c = 1; c <= s; c++) {
+                System.out.print(" ");
+            }
+            s++;
+            for (c = 1; c<= 2*(n-r)-1; c++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
 
     }
 }
